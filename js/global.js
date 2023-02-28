8f0d24e278081eab7aeafacab69935c00b64e096
ambroise recton <ambroise@recton.com>
2023-02-28 13:24:08 +0100

    video

--- js/global.js
+++ js/global.js
@@ -266,3 +266,36 @@ const observerIntersectionAnimation = () => {
 }
 
 observerIntersectionAnimation();
+
+
+var coll = document.getElementsByClassName("Mentions");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+var coll = document.getElementsByClassName("lire");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+this.classList.toggle("active");
+var content = this.nextElementSibling;
+if (content.style.display === "block") {
+  content.style.display = "none";
+} else {
+  content.style.display = "block";
+}
+});
+}
