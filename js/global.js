aaffd1da8d272fff802afbce4131caa28a1a9c58
ambroise recton <ambroise@recton.com>
2023-03-09 10:01:49 +0100

    footer + burger menu

--- js/global.js
+++ js/global.js
@@ -290,3 +290,21 @@ readMore.addEventListener("click", function () {
     content.style.display = "block";
   }
 });
+
+
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
